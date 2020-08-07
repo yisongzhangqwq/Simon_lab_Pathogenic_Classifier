ee7a3ba6f8619b96c5ee0120fba0316ca020690d
yisongzhangqwq <69355599+yisongzhangqwq@users.noreply.github.com>
2020-08-07 21:56:27 +0100

    Add files via upload

---
+++ Batch_ processing_ validator.py
@@ -0,0 +1,28 @@
+import glob
+import pandas as pd
+
+def process_file(in_file_name, out_file_name, column_name_list, indicator):
+    if indicator == 1:
+        df = pd.read_csv(in_file_name, comment="#", sep="\t")
+        df = df.dropna(subset = ["GRCh37_POS"])
+    else:
+        df = pd.read_csv(in_file_name, comment="#")
+        df = df.dropna(subset = ["Position"])
+    df.to_csv(out_file_name, index=False, header=False, float_format="%.f", columns=column_name_list, sep=" ")
+
+def main():
+    validator_filename_list = glob.glob("*_validator.txt")
+    _g_filename_list = glob.glob("*_g.csv")
+    validator_columns_list = ["GRCh37_CHR", "GRCh37_POS", "GRCh37_REF", "GRCh37_ALT"]
+    _g_columns_list = ["Chromosome", "Position", "Reference", "Alternate"]
+    for validator_filename in validator_filename_list:
+        gene_name = validator_filename.split("_")[0]
+        out_file_name = gene_name + "_HGMD.txt"
+        process_file(validator_filename, out_file_name, validator_columns_list, 1)
+    for _g in _g_filename_list:
+        gene_name = _g.split("_")[0]
+        out_file_name = gene_name + "_gnomAD.txt"
+        process_file(_g, out_file_name, _g_columns_list, 0)
+
+if __name__ == "__main__":
+    main()
